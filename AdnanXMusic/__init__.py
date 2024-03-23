1cc49086bf57d723ef356441e7dff93ad9a74c4b
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-03-23 12:13:35 +0600

    Update __init__.py

--- AdnanXMusic/__init__.py
+++ AdnanXMusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from AdnanXMusic.core.bot import Adnany
+from AdnanXMusic.core.dir import dirr
+from AdnanXMusic.core.git import git
+from AdnanXMusic.core.userbot import Userbot
+from AdnanXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -11,7 +11,7 @@ git()
 dbb()
 heroku()
 
-app = Anony()
+app = Adnany()
 userbot = Userbot()
 
 
