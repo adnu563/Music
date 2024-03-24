865b470e29fdee0b83e8b2c54c29b8fa3f05a1e7
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-03-24 13:55:24 +0600

    Update logger.py

--- AdnanXMusic/utils/logger.py
+++ AdnanXMusic/utils/logger.py
@@ -1,31 +1,33 @@
-from pyrogram.enums import ParseMode
+from config import LOG, LOG_GROUP_ID
+from YukkiMusic import app
+from YukkiMusic.utils.database import is_on_off
 
-from AdnanXMusic import app
-from AdnanXMusic.utils.database import is_on_off
-from config import LOGGER_ID
 
 async def play_logs(message, streamtype):
-    if await is_on_off(2):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "Private Group"
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+**YUKKI PLAY LOG**
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+**Chat:** {message.chat.title} [`{message.chat.id}`]
+**User:** {message.from_user.mention}
+**Username:** @{message.from_user.username}
+**User ID:** `{message.from_user.id}`
+**Chat Link:** {chatusername}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+**Query:** {message.text}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
+**StreamType:** {streamtype}"""
+        if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    chat_id=LOGGER_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
                     disable_web_page_preview=True,
                 )
-            except Exception as e:
-                print(f"Error sending message: {e}")
+            except:
+                pass
+        return
