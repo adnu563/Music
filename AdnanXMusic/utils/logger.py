4adb3591a5fb88e1ac717922c9263e9dd1379da1
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-03-24 14:29:37 +0600

    Update logger.py

--- AdnanXMusic/utils/logger.py
+++ AdnanXMusic/utils/logger.py
@@ -1,33 +1,31 @@
-from config import LOG, LOG_GROUP_ID
+from pyrogram.enums import ParseMode
 from AdnanXMusic import app
 from AdnanXMusic.utils.database import is_on_off
-
+from config import LOGGER_ID
 
 async def play_logs(message, streamtype):
-    if await is_on_off(LOG):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "Private Group"
+    if await is_on_off(2):
         logger_text = f"""
-**YUKKI PLAY LOG**
+<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {message.from_user.mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
+<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
 
-**Query:** {message.text}
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
-**StreamType:** {streamtype}"""
-        if message.chat.id != LOG_GROUP_ID:
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}
+"""
+        if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
-                    LOG_GROUP_ID,
-                    f"{logger_text}",
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
-            except:
-                pass
-        return
+            except Exception as e:
+                print(f"Error sending message: {e}")
