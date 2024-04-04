31c4dc471ce21975a456553f2b32720e374e572f
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-04 14:40:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/5acf7edd74331b916ea62.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/cb2f65c17b10bfb8f9a07.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/5acf7edd74331b916ea62.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
