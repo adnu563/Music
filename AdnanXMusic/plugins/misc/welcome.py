ac4a3c093439d6d51703910d0580c37b2f43c67f
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-04 11:49:51 +0600

    Update welcome.py

--- AdnanXMusic/plugins/misc/welcome.py
+++ AdnanXMusic/plugins/misc/welcome.py
@@ -1,23 +1,9 @@
-from telegram.ext import Updater, CommandHandler
-import logging
+def print_welcome_message(chat_name, chat_id, username, total_chat, added_by):
+    print("» ʙᴏᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ! 🥳\n")
+    print("ᴄʜᴀᴛ ɴᴀᴍᴇ:", chat_name)
+    print("ᴄʜᴀᴛ ɪᴅ:", chat_id)
+    print("ᴜsᴇʀɴᴀᴍᴇ:", username)
+    print("ᴛᴏᴛᴀʟ ᴄʜᴀᴛ:", total_chat)
+    print("ᴀᴅᴅᴇᴅ ʙʏ:", added_by)
 
-# Define action for the /start command at home
-def start(update, context):
-    chat = update.effective_chat
-    user = update.effective_user
-    added_by = update.message.from_user.first_name
-
-    # Create log message and print
-    log_message = f"» ʙᴏᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ! 🥳\n\nᴄʜᴀᴛ ɴᴀᴍᴇ: {chat.title}\nᴄʜᴀᴛ ɪᴅ: {chat.id}\nᴜsᴇʀɴᴀᴍᴇ: @{user.username}\nᴛᴏᴛᴀʟ ᴄʜᴀᴛ: {context.bot.get_chat_members_count(chat.id)}\nᴀᴅᴅᴇᴅ ʙʏ: {added_by}"
-    print(log_message)
-
-# Logging setup
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-# Create handler and add for /start command
-dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-
-# Start the bot
-updater.start_polling()
-updater.idle()
+    print_welcome_message(chat_name, chat_id, username, total_chat, added_by)
