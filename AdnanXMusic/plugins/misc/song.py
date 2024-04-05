c0c21710e15d5b78d21fc715c9fa024269cc2790
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-06 05:28:08 +0600

    Update song.py

--- AdnanXMusic/plugins/misc/song.py
+++ AdnanXMusic/plugins/misc/song.py
@@ -42,7 +42,7 @@ async def song(_, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"➻ ᴛɪᴛʟᴇ: {title[:23]}\n➻ ᴅᴜʀᴀᴛɪᴏɴ: {duration}\n➻ ᴛᴏᴛᴀʟ: {total_views}\n\n➻ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention, {"bot_mention"}}"
+        rep = f"➻ ᴛɪᴛʟᴇ: {title[:23]}\n➻ ᴅᴜʀᴀᴛɪᴏɴ: {duration}\n➻ ᴛᴏᴛᴀʟ: {total_views}\n\n➻ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention{"bot_mention"}}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
