858eed78493157e8733526da885b5bb0049d89d4
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-05 10:58:47 +0600

    Update __init__.py

--- AdnanXMusic/__init__.py
+++ AdnanXMusic/__init__.py
@@ -4,7 +4,7 @@ from AdnanXMusic.core.git import git
 from AdnanXMusic.core.userbot import Userbot
 from AdnanXMusic.misc import dbb, heroku
 from .logging import LOGGER
-from .config import @app.mention, @app.username
+from .config import BOT_MENTION, BOT_USERNAME
 
 dirr()
 git()
