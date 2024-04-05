a209aaacf58e3f52bc7f287c8d37a0d6a14a14d3
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-05 22:08:52 +0600

    Update song.py

--- AdnanXMusic/plugins/misc/song.py
+++ AdnanXMusic/plugins/misc/song.py
@@ -35,13 +35,13 @@ async def song(_, message: Message):
             f"Failed to fetch track from YT-DL.\n\nReason: `{ex}`"
         )
 
-    await m.edit_text("»⏳ 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽𝗂𝗇𝗀 𝖲𝗈𝗇𝗀, 𝖯𝗅𝖾𝖺𝗌𝖾 𝖶𝖺𝗂𝗍..!")
+    await m.edit_text("»⏳ 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖼𝗂𝗇𝗀 𝖲𝗈𝗇𝗀, 𝖯𝗅𝖾𝖺𝗌𝖾 𝖶𝖺𝗂𝗍..!")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☁️ ᴛɪᴛʟᴇ: [{title[:23]}]\n⏱️ ᴅᴜʀᴀᴛᴏɴ: {duration}\n👀 ᴛᴏᴛᴀʟ: {total_views}\n\n⏳ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention(BOT_MENTION)})"
+        rep = f"☁️ ᴛɪᴛʟᴇ: [{title[:23]}]\n⏱️ ᴅᴜʀᴀᴛᴏɴ: {duration}\n\n⏳ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention('BOT_MENTION')}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -55,12 +55,14 @@ async def song(_, message: Message):
                 title=title,
                 duration=dur,
             )
-        except:
+        except Exception as e:
+            LOGGER.error(e)
             return await m.edit_text(
                 text="Failed to upload audio on Telegram servers."
             )
         await m.delete()
-    except:
+    except Exception as e:
+        LOGGER.error(e)
         return await m.edit_text("Failed to upload audio on Telegram servers.")
 
     try:
