5edbbc4d9748b7960b8246d03fe52ebfe467fda4
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-06 04:59:41 +0600

    Update song.py

--- AdnanXMusic/plugins/misc/song.py
+++ AdnanXMusic/plugins/misc/song.py
@@ -42,12 +42,14 @@ async def song(_, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"➻ ᴛɪᴛʟᴇ: {title[:23]}\n➻ ᴅᴜʀᴀᴛɪᴏɴ: {duration}\n➻ ᴛᴏᴛᴀʟ: {total_views}\n\n➻ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention}"
-        rep = f"➻ ᴛɪᴛʟᴇ: {title[:23]}" rep = rep + f"\n➻ ᴅᴜʀᴀᴛɪᴏɴ: {duration}" rep = rep + f"\n➻ ᴛᴏᴛᴀʟ: {total_views}" rep = rep + f"\n\n➻ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention}"
+        rep = f"➻ ᴛɪᴛʟᴇ: {title[:23]}\n
+                ➻ ᴅᴜʀᴀᴛɪᴏɴ: {duration}\n
+                ➻ ᴛᴏᴛᴀʟ: {total_views}\n\n
+                ➻ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: {app.mention}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
-            secmul *= 60
+            secmul *= 45
         try:
             await app.send_audio(
                 chat_id=message.chat.id,
