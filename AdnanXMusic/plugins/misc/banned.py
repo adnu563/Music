9911c8d5b09a946ca42e27a2779a7901bf931318
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-06 11:19:31 +0600

    Update banned.py

--- AdnanXMusic/plugins/misc/banned.py
+++ AdnanXMusic/plugins/misc/banned.py
@@ -12,21 +12,6 @@ def get_logger_id():
 async def lul_message(chat_id: int, message: str):
     await app.send_message(chat_id=chat_id, text=message)
 
-@app.on_message(filters.new_chat_members)
-async def on_new_chat_members(client: Client, message: Message):
-    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.first_name if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
-        chatname = message.chat.title  # Changed matlabi_jhanto to chatname
-        served_chats = len(await get_served_chats())
-        chat_id = message.chat.id
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
-        lemda_text = f"ʙᴏᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ..!🥳\n\nᴄʜᴀᴛ ɴᴀᴍᴇ: {chatname}\nᴄʜᴀᴛ ɪᴅ: {chat_id}\nᴜsᴇʀɴᴀᴍᴇ: {chatusername}\nᴛᴏᴛᴀʟ ᴄʜᴀᴛ: {served_chats}\nᴀᴅᴅᴇᴅ ʙʏ:{added_by}"
-        logger_id = get_logger_id()  # Fetch the logger ID
-        await lul_message(logger_id, lemda_text)
-
 @app.on_message(filters.left_chat_member)
 async def on_left_chat_member(client: Client, message: Message):
     if message.left_chat_member.id == (await client.get_me()).id:
