25d874260d79e15fd6a104bc2639e96f1fe3d2f9
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-06 10:20:01 +0600

    Create welcome.py

---
+++ AdnanXMusic/plugins/misc/welcome.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from AlexaMusic import app
+from AlexaMusic.utils.database import get_served_chats
+from config import LOG_GROUP_ID
+
+
+async def lul_message(chat_id: int, message: str):
+    await app.send_message(chat_id=chat_id, text=message)
+
+
+@app.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.first_name if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        matlabi_jhanto = message.chat.title
+        served_chats = len(await get_served_chats())
+        chat_id = message.chat.id
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        lemda_text = f"🌹 ʙᴏᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ ..\n\n\n┣★ **ᴄʜᴀᴛ** › : {matlabi_jhanto}\n┣★ **ᴄʜᴀᴛ ɪᴅ** › : {chat_id}\n┣★ **ᴄʜᴀᴛ ᴜɴᴀᴍᴇ** › : {chatusername}\n┣★ **ᴛᴏᴛᴀʟ ᴄʜᴀᴛ** › : {served_chats}\n┣★ **ᴀᴅᴅᴇᴅ ʙʏ** › :\n {added_by}"
+        await lul_message(LOG_GROUP_ID, lemda_text)
