a4d5b561033961048b1349ea7a1373f359959f17
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-19 17:07:21 +0600

    Update chatgpt.py

--- AdnanXMusic/plugins/bot/chatgpt.py
+++ AdnanXMusic/plugins/bot/chatgpt.py
@@ -1,12 +1,12 @@
+from config import *
 import telebot
 import openai
-from config import *
 
 chatStr = ''
 
 def chatmodal(prompt):
     global chatStr
-    openai.api_key = OPENAI_KEY
+    openai.api_key =OPENAI_KEY
     chatStr += f"Adnan: {prompt}\nRiDi: "
     response = openai.Completion.create(
                     model="text-davinci-003",
@@ -20,21 +20,21 @@ def chatmodal(prompt):
     print(response)
     chatStr += f"{response['choices'][0]['text']}"
     return response['choices'][0]['text']
-
 bot = telebot.TeleBot(BOT_API)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(['start'])
 def start(message):
-    bot.reply_to(message, "Hello! Welcome to FarhanXMusic")
-
-@bot.message_handler(func=lambda message: True)
-def chat(message):
+    bot.reply_to(message,"hello welcome to FarhanXMusic")
+    
+@bot.message_handler()
+def chat(messagge):
     try:
         reply = chatmodal(message.text)
-        bot.reply_to(message, reply)
+        bot.reply_to(message,reply)
     except Exception as e:
         print(e)
-        bot.reply_to(message, str(e))
+        bot.reply_to(message,e)
+
 
 print("Bot Started....")
 bot.polling()
