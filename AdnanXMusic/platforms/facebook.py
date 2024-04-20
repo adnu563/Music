1f57c63db6a0b3ee35effc9ad98abca076fa6212
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-20 12:58:58 +0600

    Update facebook.py

--- AdnanXMusic/platforms/facebook.py
+++ AdnanXMusic/platforms/facebook.py
@@ -18,11 +18,14 @@ dp = Dispatcher(bot)
 
 
 async def download_video(url: str, file_name: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                with open(file_name, 'wb') as f:
-                    f.write(await resp.read())
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    with open(file_name, 'wb') as f:
+                        f.write(await resp.read())
+    except Exception as e:
+        print("Error downloading video:", e)
 
 
 @dp.message_handler(commands=['start'])
@@ -30,29 +33,32 @@ async def start(message: types.Message):
     await message.reply("Welcome! Send me a Facebook video link to download.")
 
 
-@dp.message_handler(commands=['fb'])  # Added command handler
+@dp.message_handler(commands=['fb'])
 async def download_facebook_video_command(message: types.Message):
     await download_facebook_video(message)
 
 
 @dp.message_handler(regexp=r'^(https?:\/\/)?(www\.)?facebook\.com\/.*\/videos\/.+$')
 async def download_facebook_video(message: types.Message):
-    url = message.text
-    response = requests.get(url)
-    print("Facebook API Response:", response.content)  # Debugging
-    video_url = None
-    if response.status_code == 200:
-        try:
-            video_url = response.json()['data'][0]['source']
-        except KeyError:
-            pass
-    if video_url:
-        file_name = f"video_{message.message_id}.mp4"
-        await message.reply("Downloading video...")
-        await download_video(video_url, file_name)
-        await message.reply_document(types.InputFile(file_name))
-    else:
-        await message.reply("Sorry, couldn't find a video in the provided link.")
+    try:
+        url = message.text
+        response = requests.get(url)
+        print("Facebook API Response:", response.content)  # Debugging
+        video_url = None
+        if response.status_code == 200:
+            try:
+                video_url = response.json()['data'][0]['source']
+            except KeyError:
+                pass
+        if video_url:
+            file_name = f"video_{message.message_id}.mp4"
+            await message.reply("Downloading video...")
+            await download_video(video_url, file_name)
+            await message.reply_document(types.InputFile(file_name))
+        else:
+            await message.reply("Sorry, couldn't find a video in the provided link.")
+    except Exception as e:
+        print("Error processing Facebook video link:", e)
 
 
 if __name__ == '__main__':
