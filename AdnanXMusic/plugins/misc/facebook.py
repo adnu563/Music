3ca61f33729edcab193f0d2130a3738a76a4cc9c
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-20 12:59:29 +0600

    Add files via upload

---
+++ AdnanXMusic/plugins/misc/facebook.py
@@ -0,0 +1,65 @@
+import aiohttp
+import asyncio
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+import requests
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+API_TOKEN = config['telegram']['api_token']
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+async def download_video(url: str, file_name: str):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    with open(file_name, 'wb') as f:
+                        f.write(await resp.read())
+    except Exception as e:
+        print("Error downloading video:", e)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Welcome! Send me a Facebook video link to download.")
+
+
+@dp.message_handler(commands=['fb'])
+async def download_facebook_video_command(message: types.Message):
+    await download_facebook_video(message)
+
+
+@dp.message_handler(regexp=r'^(https?:\/\/)?(www\.)?facebook\.com\/.*\/videos\/.+$')
+async def download_facebook_video(message: types.Message):
+    try:
+        url = message.text
+        response = requests.get(url)
+        print("Facebook API Response:", response.content)  # Debugging
+        video_url = None
+        if response.status_code == 200:
+            try:
+                video_url = response.json()['data'][0]['source']
+            except KeyError:
+                pass
+        if video_url:
+            file_name = f"video_{message.message_id}.mp4"
+            await message.reply("Downloading video...")
+            await download_video(video_url, file_name)
+            await message.reply_document(types.InputFile(file_name))
+        else:
+            await message.reply("Sorry, couldn't find a video in the provided link.")
+    except Exception as e:
+        print("Error processing Facebook video link:", e)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
