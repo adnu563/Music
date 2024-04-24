1d3ef8beccca44d4f38aedacb1e5d8d2c536894c
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-24 23:05:53 +0600

    Add files via upload

---
+++ AdnanXMusic/install_modules.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+echo Installing requests module...
+pip install requests
+echo.
+echo Requests module installed successfully.
+
+echo .
+echo Installing urllib  module...
+pip install urllib3
+echo.
+echo Urllib  module installed successfully.
+
+
+echo .
+echo Installing BeautifulSoup4 module...
+pip install beautifulsoup4
+echo.
+echo BeautifulSoup4 module installed successfully.
+
+echo .
+echo Installing Pillow module...
+pip install pillow
+echo.
+echo Pillow module installed successfully.
+
+pause
