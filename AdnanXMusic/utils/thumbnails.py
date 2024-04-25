b2daafad33d6204861bffc70b5dd174fa1eee1b9
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-25 10:09:48 +0600

    Update thumbnails.py

--- AdnanXMusic/utils/thumbnails.py
+++ AdnanXMusic/utils/thumbnails.py
@@ -1,147 +1,88 @@
 import os
 import re
-import textwrap
-import urllib.request
-import json
-from datetime import timedelta
-
-import requests
+import aiofiles
+import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from bs4 import BeautifulSoup
+from unidecode import unidecode
+from youtubesearchpython import VideosSearch
+from AdnanXMusic import app
+from config import YOUTUBE_IMG_URL
 
-NAME = "ADDA X MUSIC"
-
-
-def get_duration(response):
-    # Function to extract video duration
-    soup = BeautifulSoup(response, 'html.parser')
-    script_tag = soup.find('script', text=re.compile('ytInitialPlayerResponse'))
-    if script_tag:
-        video_details = re.search(r'ytInitialPlayerResponse\s*=\s*({.*?});', script_tag.string)
-        if video_details:
-            video_json = video_details.group(1)
-            video_data = json.loads(video_json)
-            if 'videoDetails' in video_data and 'lengthSeconds' in video_data['videoDetails']:
-                duration_seconds = int(video_data['videoDetails']['lengthSeconds'])
-                duration_formatted = str(timedelta(seconds=duration_seconds))
-                return duration_formatted
-
-
-def get_views(response):
-    # Function to extract views count
-    views = response.split('"shortViewCount":{"simpleText":"')[1].split('"}')[0]
-    return views
-
-
-def get_middle(duration):
-    # Function to get the middle of the video duration
-    minute = int(int(duration[1]) / 2)
-    if minute < 10:
-        minute = f"0{minute}"
-    seconds = int(int(duration[2]) / 2)
-    if seconds < 10:
-        seconds = f"0{seconds}"
-    return f"{minute} : {seconds}"
-
-
-def download_thumb(url):
-    # Function to download thumbnail
-    response = requests.get(url).text
-    image_title = response.split('<meta name="title" content="')[1].split('">')[0]
-    duration = get_duration(response)
-    views = get_views(response)
-    channel_name = response.split(', "name": "')[1].split('"}}]}')[0]
-
-    image_link = (response.split('<link rel="image_src" href="'))[1].split('">')[0]
-    image_name = image_link.split('vi/')[1].split('/')[0]
-
-    img_filename, _ = urllib.request.urlretrieve(image_link, f"assets/{image_name}.jpg")
-    img = Image.open(img_filename)
-    return image_title, image_name, duration, views, channel_name
-
-
-def edit(image_title, video_id, duration, views, channel):
-    # Function to edit thumbnail
-    image = Image.open(f"assets/{video_id}.jpg")
-    converter = ImageEnhance.Color(image)
-    image = image.filter(ImageFilter.BLUR)
-    overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-    image = Image.alpha_composite(image.convert("RGBA"), overlay)
-    draw = ImageDraw.Draw(image)
-
-    # Fonts And Color
-    font = ImageFont.truetype("assets/font.ttf", 30)
-    text_color = (255, 255, 255)
-
-    # Top Left Sight Writing
-    position = (30, 30)
-    draw.text(position, NAME, fill=text_color, font=font)
-
-    # Bottom X Y Value
-    image_width, image_height = image.size
-    x = ((image_width // 2) // 2)
-    y = (image_height // 2) + (image_height // 4)
-
-    # Title OF The Video
-    position = (x, y - 80)
-    text = textwrap.fill(f"{image_title}", width=50)
-    draw.text(position, text, fill=text_color, font=font)
-
-    # Duration Start And Close
-    if duration:
-        duritionX = duration.split(":")
-        middle_duration = get_middle(duritionX)
-        position = (x - 200, y)
-        draw.text(position, middle_duration, fill=text_color, font=font)
-
-        full_duration = f"{duritionX[1]} : {duritionX[2]}"
-        position = (x - 80 + 800, y)
-        draw.text(position, full_duration, fill=text_color, font=font)
-
-    draw.text((x + 150, y + 125), f"{channel} | {views}", fill=text_color,
-              font=ImageFont.truetype("arial.ttf", 20))
-
-    # Overlay Image
-    overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-    image = Image.alpha_composite(image.convert("RGBA"), overlay)
-    image_to_paste = Image.open("overlay.png")
-    image_to_paste = image_to_paste.convert("RGBA")
-    paste_position = (x - 80, y - 50)
-    image.paste(image_to_paste, paste_position, image_to_paste)
-
-    image.show()
-    image.save(f"assets/{video_id}_edited.png")
-
-
-def changeImageSize(maxWidth, maxHeight, image):
-    widthRatio = maxWidth / image.size[0]
-    heightRatio = maxHeight / image.size[1]
-    newWidth = int(widthRatio * image.size[0])
-    newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
-    return newImage
-
+async def change_image_size(max_width, max_height, image):
+    width_ratio = max_width / image.size[0]
+    height_ratio = max_height / image.size[1]
+    new_width = int(width_ratio * image.size[0])
+    new_height = int(height_ratio * image.size[1])
+    new_image = image.resize((new_width, new_height))
+    return new_image
 
 def clear(text):
-    list = text.split(" ")
+    words = text.split(" ")
     title = ""
-    for i in list:
-        if len(title) + len(i) < 60:
-            title += " " + i
+    for word in words:
+        if len(title) + len(word) < 60:
+            title += " " + word
     return title.strip()
 
-
-def get_thumb(videoid):
-    if os.path.isfile(f"assets/{videoid}.png"):
-        return f"assets/{videoid}.png"
+async def get_thumb(videoid):
+    if os.path.isfile(f"cache/{videoid}.png"):
+        return f"cache/{videoid}.png"
 
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            thumbnail_link = response.json()['items'][0]['snippet']['thumbnails']['maxres']['url']
-            thumbnail_filename, _ = urllib.request.urlretrieve(thumbnail_link, f"assets/{videoid}.png")
-            return thumbnail_filename
+        results = VideosSearch(url, limit=1)
+        for result in await results.next():
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    async with aiofiles.open(f"cache/thumb{videoid}.png", mode="wb") as f:
+                        await f.write(await resp.read())
+
+        youtube = Image.open(f"cache/thumb{videoid}.png")
+        image1 = await change_image_size(1280, 720, youtube)
+        image2 = image1.convert("RGBA")
+        background = image2.filter(filter=ImageFilter.BoxBlur(10))
+        enhancer = ImageEnhance.Brightness(background)
+        background = enhancer.enhance(0.5)
+        draw = ImageDraw.Draw(background)
+        arial = ImageFont.truetype("AdnanXMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("AdnanXMusic/assets/font.ttf", 30)
+        draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
+        draw.text((55, 560), f"{channel} | {views[:23]}", (255, 255, 255), font=arial)
+        draw.text((57, 600), clear(title), (255, 255, 255), font=font)
+        draw.text((36, 685), "00:00", (255, 255, 255), font=arial)
+        draw.text((1185, 685), f"{duration[:23]}", (255, 255, 255), font=arial)
+
+        # Adding overlay image
+        overlay_path = "AdnanXMusic/assets/overly.png"
+        if os.path.exists(overlay_path):
+            overlay = Image.open(overlay_path)
+            background.paste(overlay, (0, 0), overlay)
+
+        os.remove(f"cache/thumb{videoid}.png")
+
+        background.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
     except Exception as e:
         print(e)
         return YOUTUBE_IMG_URL
