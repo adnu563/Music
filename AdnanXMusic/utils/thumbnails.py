220774eae4ff796230ff121b85bcec6d4764f446
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-25 13:23:08 +0600

    Update thumbnails.py

--- AdnanXMusic/utils/thumbnails.py
+++ AdnanXMusic/utils/thumbnails.py
@@ -46,75 +46,83 @@ def get_middle(duration):
 
 def download_thumb(url):
     # Function to download thumbnail
-    response = requests.get(url).text
-    image_title = response.split('<meta name="title" content="')[1].split('">')[0]
-    duration = get_duration(response)
-    views = get_views(response)
-    channel_name = response.split(', "name": "')[1].split('"}}]}')[0]
+    try:
+        response = requests.get(url).text
+        image_title = response.split('<meta name="title" content="')[1].split('">')[0]
+        duration = get_duration(response)
+        views = get_views(response)
+        channel_name = response.split(', "name": "')[1].split('"}}]}')[0]
 
-    image_link = (response.split('<link rel="image_src" href="'))[1].split('">')[0]
-    image_name = image_link.split('vi/')[1].split('/')[0]
+        image_link = (response.split('<link rel="image_src" href="'))[1].split('">')[0]
+        image_name = image_link.split('vi/')[1].split('/')[0]
 
-    img_filename, _ = urllib.request.urlretrieve(image_link, f"AdnanXMusic/cache/{image_name}.jpg")
-    img = Image.open(img_filename)
-    return image_title, image_name, duration, views, channel_name
+        img_filename, _ = urllib.request.urlretrieve(image_link, f"AdnanXMusic/cache/{image_name}.jpg")
+        img = Image.open(img_filename)
+        return image_title, image_name, duration, views, channel_name
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 
 def edit(image_title, video_id, duration, views, channel):
     # Function to edit thumbnail
-    try:
-        image = Image.open(f"AdnanXMusic/cache/{video_id}.jpg")
-        converter = ImageEnhance.Color(image)
-        image = image.filter(ImageFilter.BLUR)
-        overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-        image = Image.alpha_composite(image.convert("RGBA"), overlay)
-        draw = ImageDraw.Draw(image)
+    if image_title and video_id and duration and views and channel:
+        try:
+            image = Image.open(f"AdnanXMusic/cache/{video_id}.jpg")
+            converter = ImageEnhance.Color(image)
+            image = image.filter(ImageFilter.BLUR)
+            overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
+            image = Image.alpha_composite(image.convert("RGBA"), overlay)
+            draw = ImageDraw.Draw(image)
 
-        # Fonts And Color
-        font = ImageFont.truetype("AdnanXMusic/assets/font.ttf", 30)
-        text_color = (255, 255, 255)
+            # Fonts And Color
+            font = ImageFont.truetype("AdnanXMusic/assets/font.ttf", 30)
+            text_color = (255, 255, 255)
 
-        # Top Left Sight Writing
-        position = (30, 30)
-        draw.text(position, NAME, fill=text_color, font=font)
+            # Top Left Sight Writing
+            position = (30, 30)
+            draw.text(position, NAME, fill=text_color, font=font)
 
-        # Bottom X Y Value
-        image_width, image_height = image.size
-        x = ((image_width // 2) // 2)
-        y = (image_height // 2) + (image_height // 4)
+            # Bottom X Y Value
+            image_width, image_height = image.size
+            x = ((image_width // 2) // 2)
+            y = (image_height // 2) + (image_height // 4)
 
-        # Title OF The Video
-        position = (x, y - 80)
-        text = textwrap.fill(f"{image_title}", width=50)
-        draw.text(position, text, fill=text_color, font=font)
+            # Title OF The Video
+            position = (x, y - 80)
+            text = textwrap.fill(f"{image_title}", width=50)
+            draw.text(position, text, fill=text_color, font=font)
 
-        # Duration Start And Close
-        if duration:
-            duritionX = duration.split(":")
-            middle_duration = get_middle(duritionX)
-            position = (x - 200, y)
-            draw.text(position, middle_duration, fill=text_color, font=font)
+            # Duration Start And Close
+            if duration:
+                duritionX = duration.split(":")
+                middle_duration = get_middle(duritionX)
+                position = (x - 200, y)
+                draw.text(position, middle_duration, fill=text_color, font=font)
 
-            full_duration = f"{duritionX[1]} : {duritionX[2]}"
-            position = (x - 80 + 800, y)
-            draw.text(position, full_duration, fill=text_color, font=font)
+                full_duration = f"{duritionX[1]} : {duritionX[2]}"
+                position = (x - 80 + 800, y)
+                draw.text(position, full_duration, fill=text_color, font=font)
 
-        draw.text((x + 150, y + 125), f"{channel} | {views}", fill=text_color,
-                  font=ImageFont.truetype("arial.ttf", 20))
+            draw.text((x + 150, y + 125), f"{channel} | {views}", fill=text_color,
+                      font=ImageFont.truetype("arial.ttf", 20))
 
-        # Overlay Image
-        overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-        image = Image.alpha_composite(image.convert("RGBA"), overlay)
-        image_to_paste = Image.open("AdnanXMusic/cache/overlay.png")
-        image_to_paste = image_to_paste.convert("RGBA")
-        paste_position = (x - 80, y - 50)
-        image.paste(image_to_paste, paste_position, image_to_paste)
+            # Overlay Image
+            overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
+            image = Image.alpha_composite(image.convert("RGBA"), overlay)
+            image_to_paste = Image.open("AdnanXMusic/cache/overlay.png")
+            image_to_paste = image_to_paste.convert("RGBA")
+            paste_position = (x - 80, y - 50)
+            image.paste(image_to_paste, paste_position, image_to_paste)
 
-        # image.show()
-        image.save(f"AdnanXMusic/cache/{video_id}_edited.png")
-        return f"assets/{video_id}_edited.png"
-    except OSError as e:
-        print(f"Error: {e}")
+            # image.show()
+            image.save(f"AdnanXMusic/cache/{video_id}_edited.png")
+            return f"assets/{video_id}_edited.png"
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+    else:
+        print("Error: Insufficient data to edit thumbnail.")
         return None
 
 
