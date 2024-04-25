684c12876f22897449add608a62e6e8f0d3d7cae
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-25 13:34:17 +0600

    Update thumbnails.py

--- AdnanXMusic/utils/thumbnails.py
+++ AdnanXMusic/utils/thumbnails.py
@@ -1,137 +1,121 @@
 import os
 import re
-import textwrap
-import urllib.request
-import json
-from datetime import timedelta
 
-import requests
+import aiofiles
+import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from bs4 import BeautifulSoup
-import logging
+from unidecode import unidecode
+from youtubesearchpython.future import VideosSearch
 
-NAME = "app.name"
-
-
-def get_duration(response):
-    # Function to extract video duration
-    soup = BeautifulSoup(response, 'html.parser')
-    script_tag = soup.find('script', text=re.compile('ytInitialPlayerResponse'))
-    if script_tag:
-        video_details = re.search(r'ytInitialPlayerResponse\s*=\s*({.*?});', script_tag.string)
-        if video_details:
-            video_json = video_details.group(1)
-            video_data = json.loads(video_json)
-            if 'videoDetails' in video_data and 'lengthSeconds' in video_data['videoDetails']:
-                duration_seconds = int(video_data['videoDetails']['lengthSeconds'])
-                duration_formatted = str(timedelta(seconds=duration_seconds))
-                return duration_formatted
+from AdnanXMusic import app
+from config import YOUTUBE_IMG_URL
 
 
-def get_views(response):
-    # Function to extract views count
-    views = response.split('"shortViewCount":{"simpleText":"')[1].split('"}')[0]
-    return views
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
 
 
-def get_middle(duration):
-    # Function to get the middle of the video duration
-    minute = int(int(duration[1]) / 2)
-    if minute < 10:
-        minute = f"0{minute}"
-    seconds = int(int(duration[2]) / 2)
-    if seconds < 10:
-        seconds = f"0{seconds}"
-    return f"{minute} : {seconds}"
+def clear(text):
+    list = text.split(" ")
+    title = ""
+    for i in list:
+        if len(title) + len(i) < 60:
+            title += " " + i
+    return title.strip()
 
 
-def download_thumb(url):
-    # Function to download thumbnail
+async def get_thumb(videoid):
+    if os.path.isfile(f"cache/{videoid}.png"):
+        return f"cache/{videoid}.png"
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
     try:
-        response = requests.get(url).text
-        image_title = response.split('<meta name="title" content="')[1].split('">')[0]
-        duration = get_duration(response)
-        views = get_views(response)
-        channel_name = response.split(', "name": "')[1].split('"}}]}')[0]
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
 
-        image_link = (response.split('<link rel="image_src" href="'))[1].split('">')[0]
-        image_name = image_link.split('vi/')[1].split('/')[0]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    await f.write(await resp.read())
+                    await f.close()
 
-        img_filename, _ = urllib.request.urlretrieve(image_link, f"AdnanXMusic/cache/{image_name}.jpg")
-        img = Image.open(img_filename)
-        return image_title, image_name, duration, views, channel_name
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-
-def edit(image_title, video_id, duration, views, channel):
-    # Function to edit thumbnail
-    if all(param is not None for param in [image_title, video_id, duration, views, channel]):
+        youtube = Image.open(f"cache/thumb{videoid}.png")
+        image1 = changeImageSize(1280, 720, youtube)
+        image2 = image1.convert("RGBA")
+        background = image2.filter(filter=ImageFilter.BoxBlur(10))
+        enhancer = ImageEnhance.Brightness(background)
+        background = enhancer.enhance(0.5)
+        draw = ImageDraw.Draw(background)
+        arial = ImageFont.truetype("AdnanXMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("AdnanXMusic/assets/font.ttf", 30)
+        draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
+        draw.text(
+            (55, 560),
+            f"{channel} | {views[:23]}",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (57, 600),
+            clear(title),
+            (255, 255, 255),
+            font=font,
+        )
+        draw.line(
+            [(55, 660), (1220, 660)],
+            fill="white",
+            width=5,
+            joint="curve",
+        )
+        draw.ellipse(
+            [(918, 648), (942, 672)],
+            outline="white",
+            fill="white",
+            width=15,
+        )
+        draw.text(
+            (36, 685),
+            "00:00",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (1185, 685),
+            f"{duration[:23]}",
+            (255, 255, 255),
+            font=arial,
+        )
         try:
-            image = Image.open(f"AdnanXMusic/cache/{video_id}.jpg")
-            converter = ImageEnhance.Color(image)
-            image = image.filter(ImageFilter.BLUR)
-            overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-            image = Image.alpha_composite(image.convert("RGBA"), overlay)
-            draw = ImageDraw.Draw(image)
-
-            # Fonts And Color
-            font = ImageFont.truetype("AdnanXMusic/assets/font.ttf", 30)
-            text_color = (255, 255, 255)
-
-            # Top Left Sight Writing
-            position = (30, 30)
-            draw.text(position, NAME, fill=text_color, font=font)
-
-            # Bottom X Y Value
-            image_width, image_height = image.size
-            x = ((image_width // 2) // 2)
-            y = (image_height // 2) + (image_height // 4)
-
-            # Title OF The Video
-            position = (x, y - 80)
-            text = textwrap.fill(f"{image_title}", width=50)
-            draw.text(position, text, fill=text_color, font=font)
-
-            # Duration Start And Close
-            if duration:
-                duritionX = duration.split(":")
-                middle_duration = get_middle(duritionX)
-                position = (x - 200, y)
-                draw.text(position, middle_duration, fill=text_color, font=font)
-
-                full_duration = f"{duritionX[1]} : {duritionX[2]}"
-                position = (x - 80 + 800, y)
-                draw.text(position, full_duration, fill=text_color, font=font)
-
-            draw.text((x + 150, y + 125), f"{channel} | {views}", fill=text_color,
-                      font=ImageFont.truetype("arial.ttf", 20))
-
-            # Overlay Image
-            overlay = Image.new("RGBA", image.size, (50, 50, 50, 50))
-            image = Image.alpha_composite(image.convert("RGBA"), overlay)
-            image_to_paste = Image.open("AdnanXMusic/cache/overlay.png")
-            image_to_paste = image_to_paste.convert("RGBA")
-            paste_position = (x - 80, y - 50)
-            image.paste(image_to_paste, paste_position, image_to_paste)
-
-            # image.show()
-            image.save(f"AdnanXMusic/cache/{video_id}_edited.png")
-            return f"assets/{video_id}_edited.png"
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
-    else:
-        print("Error: Insufficient data to edit thumbnail.")
-        return None
-
-
-def get_thumb(videoid):
-    url = f'https://www.youtube.com/watch?v={videoid}'
-    data = download_thumb(url)
-    if data:
-        done_image = edit(*data)
-        return done_image 
-    else:
-        return None
+            os.remove(f"cache/thumb{videoid}.png")
+        except:
+            pass
+        background.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception as e:
+        print(e)
+        return YOUTUBE_IMG_URL
