29eda70f4169262bcf8a40aa15f4037ac2caf441
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-29 17:29:56 +0600

    Delete AdnanXMusic/plugins/misc/config.py

--- AdnanXMusic/plugins/misc/config.py
+++
@@ -1,6 +0,0 @@
-API_ID = '27913018' 
-# Replace 'your_api_id' with your actual Telegram API ID.
-API_HASH = '04e2f4e414cdabe52ad985adaa6cfe09'  
-# Replace 'your_api_hash' with your actual Telegram API HASH.
-BOT_TOKEN = '7091541433:AAHHwt431VqrI3jkYPaEMDmCxZ9PFTvs5f8'  
-# Replace 'your_bot_token' with your actual Telegram BOT token.
