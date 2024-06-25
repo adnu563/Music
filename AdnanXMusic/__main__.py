109f439e961b95b2300a69c66b3f8bdb3891fac1
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-06-25 11:54:28 +0600

    Update __main__.py

--- AdnanXMusic/__main__.py
+++ AdnanXMusic/__main__.py
@@ -35,7 +35,7 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AdnanXMusic.plugins" + all_module)
+        importlib.import_module("AdnanXMusic.plugins." + all_module)
     LOGGER("AdnanXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
@@ -43,7 +43,7 @@ async def init():
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AdnanXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
     except:
@@ -51,10 +51,11 @@ async def init():
     await Anony.decorators()
     LOGGER("AdnanXMusic").info(
         "\x41\x64\x6e\x61\x6e\x58\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x20\x44\x6f\x6e\x27\x74\x20\x46\x6f\x72\x67\x65\x74\x20\x54\x6f\x20\x56\x69\x73\x69\x74\x20\x40\x4d\x75\x73\x69\x63\x42\x6f\x74\x47\x68\x6f\x72\x20\x26\x20\x40\x4d\x75\x73\x69\x63\x47\x68\x6f\x72"
+    )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AdnanXMusic").info("Stopping AnonX Music Bot...")
+    LOGGER("AdnanXMusic").info("Stopping AdnanX Music Bot...")
 
 
 if __name__ == "__main__":
